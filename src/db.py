defec30ee6ecc0ddfe9bc334a83df98e12f0c34e
Ramil Mavlutov <stirk2281337@gmail.com>
2023-10-04 15:47:43 +0500

    Configure main stack

---
+++ src/db.py
@@ -0,0 +1,20 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+from config import settings
+
+Base = declarative_base()
+
+DATABASE_URL: str = str(settings.postgres)
+
+
+engine = create_async_engine(DATABASE_URL)
+async_session_maker = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
